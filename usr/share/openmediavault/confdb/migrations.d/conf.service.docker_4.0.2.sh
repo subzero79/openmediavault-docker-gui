5b616410fb86290a5e969d078d2b72844d34a86a
subzero79 <subzero79@users.noreply.github.com>
2018-02-13 11:28:09 +1000

    Move the deletion of fstab bind into migrate script
    Fix some install/remove issues

--- usr/share/openmediavault/confdb/migrations.d/conf.service.docker_4.0.2.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.docker_4.0.2.sh
@@ -25,7 +25,44 @@ set -e
 
 SERVICE_XPATH_NAME="docker"
 SERVICE_XPATH="/config/services/${SERVICE_XPATH_NAME}"
+OMV_FSTAB_DM_NAME="conf.system.filesystem.mountpoint"
+OMV_DOCKER_BIND_MOUNT="/var/lib/docker/openmediavault"
 
+clean_docker_mount_bind () {
+  echo "Deleting fstab entry ${OMV_DOCKER_BIND_UUID} from the database..."
+  omv-confdbadm delete --uuid ${OMV_DOCKER_BIND_MOUNT_UUID} ${OMV_FSTAB_DM_NAME}
+  echo "Regenerating fstab file..."
+  omv-mkconf fstab
+}
+
+### Stop the docker daemon
+systemctl stop docker.socket
+systemctl stop docker
+
+### After this section we need to remove fstab entries and unmount any old binds
+### Compare the old default bind path against fstab database to see if this configuration exists.
+OMV_DOCKER_BIND_MOUNT_UUID=$(omv-confdbadm read ${OMV_FSTAB_DM_NAME} | \
+                              jq -r \
+                              --arg bind_path $OMV_DOCKER_BIND_MOUNT \
+                              '.[]|select(.dir == $bind_path )|.uuid')
+
+### if the uuid bind exists in the database then we proceed
+if [ ! -z "$OMV_DOCKER_BIND_MOUNT_UUID" ] ;then
+	echo "There is an alternate Docker root directory defined in the plugin and is mounted, proceeding to unmount..."
+### Check if mounted
+  	if mountpoint -q -- "${OMV_DOCKER_BIND_MOUNT}"; then
+    	echo "Unmounting ${OMV_DOCKER_BIND_MOUNT}"
+    	umount -f -l "${OMV_DOCKER_BIND_MOUNT}"
+    	clean_docker_mount_bind
+	else
+    	echo "The Docker root directory is not mounted..."
+    	clean_docker_mount_bind
+  	fi
+else
+	echo "No alternative Docker root directory registered in the database..."
+fi
+
+### Delete old deprecated database entries
 
 if omv_config_exists "${SERVICE_XPATH}/apiPort"; then
     omv_config_delete "${SERVICE_XPATH}/apiPort"
@@ -56,4 +93,9 @@ if omv_config_exists "${SERVICE_XPATH}/destpath"; then
 fi
 
 
+### Regenerate configuration for the daemon
+echo "Regenerating the plugin settings for docker root directory in /etc/default/docker"
+omv-mkconf docker
 
+### Start the docker daemon 
+systemctl start docker
